762b1b2d329acb3214eb16331c7933a417354911
Arthur Rodrigues <121518722+ArthurJRCelso@users.noreply.github.com>
2023-04-23 19:27:27 +0100

    revisão

---
+++ Alogritmos e lógica de programação/01/script.js
@@ -0,0 +1,10 @@
+/* 
+    Econtre a solução ao poroblema:
+
+    Pergunte o nome do usuário e escreva a mensagem:
+    "Olá, [nome do usuário]" 
+*/
+
+var nome = prompt('Qual é o seu nome?')
+alert(`Olá, ${nome}`)
+
---
+++ Alogritmos e lógica de programação/02/script.js
@@ -0,0 +1,10 @@
+/*
+    Solicite dois números, faça a soma deles
+    e apresente o resultado final ao usuário
+*/
+
+alert("Iremos somar dois números")
+let numberOne = Number(prompt("Digite o primeiro número:"))
+let numberTwo = Number(prompt("Digite o segundo número:"))
+let result = numberOne + numberTwo
+alert(result)
