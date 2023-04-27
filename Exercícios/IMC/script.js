d93a04dbd38e5e9d8157b1c8db59e16157eca8dd
Arthur Rodrigues <121518722+ArthurJRCelso@users.noreply.github.com>
2023-04-27 21:11:04 +0100

    Modal

--- Exercícios/IMC/script.js
+++ Exercícios/IMC/script.js
@@ -2,12 +2,18 @@ const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
-const modalWrapper = document.querySelector('.modal-wrapper')
-const modalMessage = document.querySelector('.modal .title span')
-const modalBtnClose = document.querySelector('.modal button.close')
-
 const Modal = {
-    
+
+    wrapper: document.querySelector('.modal-wrapper'),
+    message: document.querySelector('.modal .title span'),
+    buttonClose: document.querySelector('.modal button.close'),
+
+    open() {
+        Modal.wrapper.classList.add('open')
+    },
+    close() {
+        Modal.wrapper.classList.remove('open')
+    },
 }
 
 form.onsubmit = function(event) {
@@ -19,12 +25,12 @@ form.onsubmit = function(event) {
     const result = IMC(weight, height)
     const message = `Seu IMC é de ${result}`
 
-    modalMessage.innerHTML = message
-    modalWrapper.classList.add('open')
+    Modal.message.innerHTML = message
+    Modal.open()
 }
 
-modalBtnClose.onclick = function() {
-    modalWrapper.classList.remove('open')
+    Modal.buttonClose.onclick = function() {
+    Modal.close()
 }
 
 function IMC(weight, height) {
